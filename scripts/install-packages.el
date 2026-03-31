1b13095a3a238df0dbc16cc350f137f170d8fc10
Mike Olson <mwolson@member.fsf.org>
2026-03-31 15:59:19 -0400

    Fix renamed script bootstrap loading

--- scripts/install-packages.el
+++ scripts/install-packages.el
@@ -1,10 +1,12 @@
 ;;; install-packages.el --- Install packages declared by an Emacs config -*- lexical-binding: t -*-
 
 (eval-and-compile
-  (let ((source-file (or load-file-name byte-compile-current-file buffer-file-name)))
-    (load (expand-file-name "../vcupp-batch.el"
-                            (file-name-directory source-file))
-          nil nil t)))
+  (let* ((source-file (or load-file-name buffer-file-name byte-compile-current-file))
+         (source-dir (and source-file
+                          (file-name-directory (expand-file-name source-file)))))
+    (when source-dir
+      (add-to-list 'load-path (expand-file-name ".." source-dir)))
+    (require 'vcupp-batch)))
 
 (vcupp-batch-install-packages)
 
